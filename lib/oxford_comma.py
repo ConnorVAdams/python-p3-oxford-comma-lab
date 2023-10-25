6e8b572c1e0947c0940ee390d11230d8e638af41
Connor Adams <connorvadams@gmail.com>
2023-10-24 19:44:39 -0600

    Submits lab.

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[-1]
+    elif len(items) == 2:
+        return f'{items[-2]} and {items[-1]}'
+    elif len(items) > 2:
+        last_two = f'{items[-2]}, and {items[-1]}'
+        string = f'{", ".join(items[0:-2])}, {last_two}'
+        return string
+
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
